4e2148d58214d2678a4edbe4bf7e9251ae1293f5
Shelby <vanhooser@ou.edu>
2014-12-28 18:21:00 -0800

    Updated routes

--- routes/index.js
+++ routes/index.js
@@ -12,9 +12,34 @@ exports.index = function(req, res){
 };
 
 exports.update = function(req, res){
-	res.json("update");
+	var updates = db.get('updates');
+	var entry = {
+		time : req.body.time,
+		packCurrent : req.body.packCurrent,
+		packOpenVoltage : req.body,packOpenVoltage,
+		packSummedVoltage : req.body.packSummedVoltage,
+		packSOC : req.body.packSOC,
+		packHealth : req.body.packHealth,
+		highCellVoltage : req.body.highCellVoltage,
+		lowCellVoltage : req.body.lowCellVoltage,
+		avgCellVoltage : req.body.avgCellVoltage,
+		highTemperature : req.body.highTemperature,
+		lowTemperature : req.body.lowTemperature,
+		maxPackDCL : req.body.maxPackDCL,
+		maxPackCCL : req.body.maxPackCCL
+	};
+	updates.update({_id:req.body.id}, entry, function(err, records){
+		if(err){
+			res.json("ERROR");
+		} else {
+			res.json("Success");
+		}
+	});
 };
 
 exports.all = function(req, res){
-	res.json("all");
+	var updates = db.get('updates');
+	updates.find({},{}, function(e, docs){
+                res.json(docs);
+        });
 };
